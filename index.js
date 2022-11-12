06dc851953a1f9653c13273756e67fae77b82bf6
amendoxe <arturo_mendoza@outlook.com>
2022-11-11 21:28:03 -0600

    Create cuentas folder and  move all related files there

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*Importación de clases*/
 import { Cliente } from "./Cliente.js";
-import { CuentaCorriente } from "./CuentaCorriente.js";
-import { CuentaAhorro } from "./CuentaAhorro.js";
-import { CuentaNomina } from "./CuentaNomina.js";
+import { CuentaCorriente } from "./cuentas/CuentaCorriente.js";
+import { CuentaAhorro } from "./cuentas/CuentaAhorro.js";
+import { CuentaNomina } from "./cuentas/CuentaNomina.js";
 
 const cliente = new Cliente("Leonardo", "13804050", "123224");
 const cliente2 = new Cliente("María", "16979808", "8989");
